c64b7a4bfb3760cca06e5760479c765230538cd9
Lucas <lucasramos.m@gmail.com>
2020-07-03 18:07:51 -0300

    Initial Commit

---
+++ ModuloActionsiOS/ModuloActionsiOS.h
@@ -0,0 +1,19 @@
+//
+//  ModuloActionsiOS.h
+//  ModuloActionsiOS
+//
+//  Created by Lucas on 03/07/20.
+//  Copyright © 2020 Lucas. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ModuloActionsiOS.
+FOUNDATION_EXPORT double ModuloActionsiOSVersionNumber;
+
+//! Project version string for ModuloActionsiOS.
+FOUNDATION_EXPORT const unsigned char ModuloActionsiOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ModuloActionsiOS/PublicHeader.h>
+
+
